ae81e010b11eba7b9f802feecc33be246885e08d
takabosoft <takabosoft@gmail.com>
2025-02-23 09:33:25 +0900

    コース定義

--- src/index/index.ts
+++ src/index/index.ts
@@ -9,6 +9,7 @@
 import { World, Testbed, Box, Vec2, Body, PolygonShape, RevoluteJoint, Edge, Chain } from "planck/with-testbed";
 import { Car } from "../common/car/car";
 import { ControlState } from "../common/car/controlState";
+import { TestCourse } from "../common/course/course";
 
 
 
@@ -38,50 +39,24 @@ class HumanPlayer {
 $(() => {
     console.log("OK");
 
-    // Construct a world object, which will hold and simulate the rigid bodies.
     const world = new World({
         gravity: new Vec2(0, 0),
     });
 
-
-    // Call the body factory which allocates memory for the ground body
-    // from a pool and creates the ground box shape (also from a pool).
-    // The body is also added to the world.
-    /*const groundBody = world.createBody({
-        position: new Vec2(0, -10),
-    });
-
-    // Add the ground fixture to the ground body.
-    groundBody.createFixture({
-        shape: new Box(20.0, 1.0),
-        density: 0.0,
-        friction: 0.9,
-    });*/
-
-    //const boundaryBody = world.createBody();
-
-    const boundaryChain = world.createBody();
-    boundaryChain.createFixture(new Chain([
-        new Vec2(-10, -5), // 左下
-        new Vec2( 0, -6),
-        new Vec2(10, -5),
-        new Vec2(10, 5),
-        new Vec2(-10, 5),
-        new Vec2(-10, -5),
-    ].map(v => v.mul(15))), { friction: 1.0 });
+    const testCourse = new TestCourse(world);
 
     const testPlayer = new HumanPlayer();
 
     //const tire = new Tire(world, testPlayer.controlState);
 
     const car = new Car(world, testPlayer.controlState);
-    car.reset(new Vec2(0, 0), Math.PI / 2);
+    car.reset(testCourse.startPos, Math.PI / 2);
 
     const testbed = Testbed.mount();
-    testbed.x = 0;
-    testbed.y = 0;
-    testbed.width = 300;
-    testbed.height = 300;
+    testbed.x = -testCourse.size.x / 2;
+    testbed.y = -testCourse.size.y / 2;
+    testbed.width = testCourse.size.x;
+    testbed.height = testCourse.size.y;
 
     testbed.start(world);
     testbed.keydown = (keyCode, label) => {
